d1009184c76445f4a5652a9e413178f4237ea555
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-06-01 03:12:17 +0600

    Server side setup deone

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 5000;
+
+
+// middlewers
+app.use(cors());
+app.use(express.json())
+
+
+
+
+//custom middleweres
+const verifyToken = async (req, res, next) => {
+    console.log('Inside verify token', req.headers.authorization)
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ message: 'Unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+// verify admin after verify token
+const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email }
+    const user = await userCollection.findOne(query)
+    let isAdmin = user?.role === 'admin';
+    if (!isAdmin) {
+        return res.status(403).send({ message: 'Forbidden access' })
+    }
+    next();
+}
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oh0s98i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //   await client.connect();
+
+        const userCollection = client.db("ResidenceProDB").collection("users");
+
+        //! API's
+
+        //jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        app.get('/users',async(req,res)=>{
+            res.send(await userCollection.find(req.body).toArray())
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        //   await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
