83d7915f5fb13aad8e991f81fdf74990a757a149
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-06-07 20:02:44 +0600

    middlewares added

--- index.js
+++ index.js
@@ -3,52 +3,53 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
+// middlewares
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-// middlewers
-app.use(cors());
-app.use(express.json())
+// custom middlewares
+const logger = async (req, res, next) => {
+    console.log('Called', req.host, req.originalUrl);
+    next();
+};
 
-
-
-
-//custom middleweres
 const verifyToken = async (req, res, next) => {
-    console.log('Inside verify token', req.headers.authorization)
-    if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'Unauthorized access' })
+    const token = req.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
     }
-    const token = req.headers.authorization.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKN_SECRET, (error, decoded) => {
-        if (error) {
-            return res.status(401).send({ message: 'Unauthorized access' })
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            console.log(err);
+            return res.status(401).send({ message: 'unauthorized access' });
         }
-        req.decoded = decoded;
+        req.user = decoded;
         next();
-    })
-}
+    });
+};
 
 // verify admin after verify token
 const verifyAdmin = async (req, res, next) => {
-    const email = req.decoded.email;
-    const query = { email: email }
-    const user = await userCollection.findOne(query)
+    const email = req.user.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
     let isAdmin = user?.role === 'admin';
     if (!isAdmin) {
-        return res.status(403).send({ message: 'Forbidden access' })
+        return res.status(403).send({ message: 'Forbidden access' });
     }
     next();
-}
-
-
-
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oh0s98i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -56,67 +57,80 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+
+let userCollection;
+let roomCollection;
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        //   await client.connect();
+        userCollection = client.db("ResidenceProDB").collection("users");
+        roomCollection = client.db("ResidenceProDB").collection("rooms");
 
-        const userCollection = client.db("ResidenceProDB").collection("users");
-        const roomCollection = client.db("ResidenceProDB").collection("rooms");
-
-        //! API's
-
-
-        //jwt related api
+        // AUTH related API
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKN_SECRET, { expiresIn: '1h' });
-            res.send({ token });
-        })
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true });
+        });
+
+        // if invallid token then clear jwt
+        app.post('/clear-jwt', (req, res) => {
+            res.clearCookie('token', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            }).send({ success: true });
+        });
 
         // post user if not existing user
         app.post('/users', async (req, res) => {
             const user = req.body;
             const emailQuery = { email: user.email };
             const existingUser = await userCollection.findOne(emailQuery);
+            // if (existingUser) {
+            //     return res.status(403).send({ message: 'User already Exists' });
+            // }
             if (!existingUser) {
-                res.status(403).send({ message: 'User already Exists' })
+                const result = await userCollection.insertOne(user);
+                res.send({ result });
             }
-            const result = await userCollection.insertOne(user);
-            res.send({ result })
+        });
 
-        })
 
         // get all users
         app.get('/users', async (req, res) => {
-            res.send(await userCollection.find(req.body).toArray())
-        })
+            res.send(await userCollection.find(req.body).toArray());
+        });
 
         // get all rooms
-        app.get('/rooms', async (req, res) => {
-            res.send(await roomCollection.find(req.body).toArray())
-        })
+        app.get('/rooms', logger, verifyToken, async (req, res) => {
+            // console.log('user in the valid token', req.user);
+            // if (req.query.email !== req.user.email) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+            // let query = {};
+            // if (req.query?.email) {
+            //     query = { email: req.query.email };
+            // }
+            res.send(await roomCollection.find(req.body).toArray());
+        });
 
-
-
-
-        // Send a ping to confirm a successful connection
-        //   await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        //   await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
-
-
-
-
 app.get('/', (req, res) => {
-    res.send('Server is running')
-})
+    res.send('Server is running');
+});
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
