be6a5963af1138dcb3997b26eecf1250dd842b37
Muhammad Saif <muhammadsaif7717@gmail.com>
2024-06-09 23:32:17 +0600

    Added firebase origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://residencepro-7717.web.app','https://residencepro-7717.firebaseapp.com'],
     credentials: true,
 }));
 app.use(express.json());
